9cb57ba9a2a7981e2fcd6110dbeadb78c472d3d1
DimKush <demonmadman228@gmail.com>
2020-04-30 13:45:16 +0300

    01-CON | Integration conan with travis ci #6

--- gtest.cmake
+++ gtest.cmake
@@ -4,13 +4,13 @@ message("${GOOGLETEST_ROOT}")
 message("${PROJECT_SOURCE_DIR}")
 
 include_directories(SYSTEM
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/include
+    ${PROJECT_SOURCE_DIR}/googletest/${GOOGLETEST_ROOT}
+    ${PROJECT_SOURCE_DIR}/googletest/${GOOGLETEST_ROOT}/include
     )
 
 set(GOOGLETEST_SOURCES
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
+    ${PROJECT_SOURCE_DIR}/googletest/${GOOGLETEST_ROOT}/src/gtest-all.cc
+    ${PROJECT_SOURCE_DIR}/googletest/${GOOGLETEST_ROOT}/src/gtest_main.cc
     )
 
 foreach(_source ${GOOGLETEST_SOURCES})
